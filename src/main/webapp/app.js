45e0f814654766686842cf85c7f71991300a8002
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2023-11-24 09:55:59 -0400

    cambios en el frontend

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,22 +1,43 @@
 
-const poliza = document.getElementById("poliza").value;
+const poliza = document.getElementById("poliza");
 const form = document.getElementById("form");
+const button = document.getElementById("btn-enviar");
+const errorElement = document.getElementById("Poliza-error");
+const cardElement = document.getElementById("card");
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    validarPoliza();
-    console.log("enviado");
+    if (poliza.value === "" || poliza.value.length < 3) {
+        errorElement.innerHTML = `<p style="color:red;"> Verifica Tu poliza de seguros </p>`;
+        return false;
+    }
+    console.log(poliza.value)
+    errorElement.innerHTML = `<p style="color:green;"> Datos correctos</p>`;
+    data(poliza.value, card);
+    return true;
+
+    //resetar el formulario
 
 });
 
- const validarPoliza = () => {
-
-    if (poliza.length < 3) {
-        document.getElementById("Poliza-error").innerHTML = `<p style="color:red;">
-                                                                El campo debe tener al menos 3 caracteres</p>`;
+function validarPoliza() {
+    if (poliza === "" || poliza.length < 3) {
+        errorElement.innerHTML = `<p style="color:red;"> Datos incorrectos</p>`;
         return false;
-    } else {
-        return true;
     }
+    console.log(poliza)
+    return true;
+}
 
+
+
+const data = (poliza, card) => {
+    cardElement.innerHTML =
+        `<div class="card" style="width: 18rem;">
+            <div class="card-body"> 
+                <h5 class="card-title">Poliza</h5> 
+                <p class="card-text"> ${poliza} </p> 
+            </div> 
+        </div>`;
 }
