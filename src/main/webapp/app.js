a116393f7f4d04b7b57cc7cca879a0bd297fc204
elizonRL <elizorodriguez16@gmail.com>
2023-11-25 01:38:16 -0400

    face final

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -6,23 +6,43 @@ const button = document.getElementById("btn-enviar");
 const errorElement = document.getElementById("Poliza-error");
 const cardElement = document.getElementById("card");
 
-
+let errorMessa = `<p style="color:red;"> Verifica Tu poliza de seguros </p>`;
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    if (poliza.value === "" || poliza.value.length < 3) {
-        errorElement.innerHTML = `<p style="color:red;"> Verifica Tu poliza de seguros </p>`;
+
+    const request = await fetch(`http://localhost:8081/poliza?num_poliza=${poliza.value}`);
+    const datos = await request.json();
+
+    if (poliza.value === "" || poliza.value.length < 3 || !datos.length) {
+        errorElement.innerHTML = errorMessa ;
+        cardElement.innerHTML= "";
+        poliza.value ="";
         return false;
     }
-    const request = await fetch(`http://localhost:8081/poliza?num_poliza=123456`);
-    const datos = await request.json();
+    console.log(datos)
+    dataProsesada(datos);
     console.log(poliza.value); // {num_poliza: 123456, nombre: "Juan", apellido: "Perez"}
     errorElement.innerHTML = `<p style="color:green;"> Datos correctos</p>`;
-    data(poliza.value, card);
+    data(poliza.value);
+    poliza.value="";
     return true;
 
     //resetar el formulario
 
 });
+
+const dataProsesada= (datos)=>{
+    datos.forEach((element)=>{
+      fech_ini = element["fecha_inicio"]
+      fech_fin = element["fecha_fin"];
+      cliente = element["cliente"]
+      vehiculos = element["vehiculos"]
+      prima = element["prima"]
+        tipo_poliza= element["tipo_poliza"]
+        console.log(vehiculos.map((e)=> e["modelo"]), fech_ini, element
+        )
+    })
+}
 
 function validarPoliza() {
     if (poliza === "" || poliza.length < 3) {
@@ -35,7 +55,7 @@ function validarPoliza() {
 
 
 
-const data = (poliza, card) => {
+const data = (poliza) => {
     cardElement.innerHTML =
         ` <div class="containerCadr">
             <div class="headerCard">
@@ -45,27 +65,39 @@ const data = (poliza, card) => {
                 <table class="tableCard">
                     <tr>
                         <th>Póliza</th>
-                        <td>AUTO12345678</td>
+                        <td>${poliza}</td>
+                    </tr>
+                    <tr>
+                        <th>Tipo de Poliza</th>
+                        <td>${tipo_poliza}</td>
                     </tr>
                     <tr>
                         <th>Nombre</th>
-                        <td>John Doe</td>
+                        <td>${cliente.nombre +" "+cliente.apellido}</td>
                     </tr>
                     <tr>
                         <th>Número de vehículos</th>
-                        <td>2</td>
+                        <td>${vehiculos.length}</td>
+                    </tr>
+                    <tr>
+                        <th>Marca</th>
+                        <td>${vehiculos.map((e)=> e["marca"])}</td>
                     </tr>
                     <tr>
                         <th>Modelo</th>
-                        <td>Toyota Camry</td>
+                        <td>${vehiculos.map((e)=> e["modelo"])}</td>
                     </tr>
                     <tr>
                         <th>Fecha de Inicio</th>
-                        <td>10-10-2023</td>
+                        <td>${fech_ini}</td>
                     </tr>
                     <tr>
                         <th>Fecha de Finalización</th>
-                        <td>10-10-2024</td>
+                        <td>${fech_fin}</td>
+                    </tr>
+                    <tr>
+                        <th>Prima</th>
+                        <td>${prima}</td>
                     </tr>
                 </table>
             </div>
